277b772972c08ccf65feedcc5932fc2ddafc4f44
oktaviannalle <oktavian.putra@formulatrix.com>
2026-08-12 22:01:34 +0700

    feat: add MaintenanceScheduleFactory for randomized maintenance plans

---
+++ backend/database/factories/MaintenanceScheduleFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\MaintenanceSchedule;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<MaintenanceSchedule>
+ */
+class MaintenanceScheduleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'asset_id' => Asset::inRandomOrder()->first()?->id ?? Asset::factory(),
+            'maintenance_type' => fake()->randomElement([
+                'Servis AC', 'Cek instalasi listrik', 'Pengecatan ulang', 'Kalibrasi alat',
+            ]),
+            'frequency_months' => fake()->randomElement([1, 3, 6, 12]),
+            'next_due_date' => fake()->dateTimeBetween('now', '+6 months'),
+        ];
+    }
+}
