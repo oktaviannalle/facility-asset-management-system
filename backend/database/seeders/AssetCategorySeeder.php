a1b8cf976cf508f481704e1872b6d31b696066f4
oktaviannalle <oktavian.putra@formulatrix.com>
2026-08-12 21:57:45 +0700

    feat: add AssetCategorySeeder with fixed reference data

---
+++ backend/database/seeders/AssetCategorySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AssetCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+  public function run(): void
+    {
+        $categories = [
+            ['name' => 'Elektronik', 'description' => 'AC, komputer, proyektor, dan perangkat elektronik lainnya'],
+            ['name' => 'Mebel', 'description' => 'Meja, kursi, lemari, dan perabot lainnya'],
+            ['name' => 'Bangunan', 'description' => 'Komponen gedung seperti atap, pintu, jendela'],
+            ['name' => 'Kendaraan', 'description' => 'Kendaraan operasional kampus'],
+        ];
+
+        foreach ($categories as $category) {
+            AssetCategory::create($category);
+        }
+    }
+}
