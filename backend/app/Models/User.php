ff6effd6bf02b9514babfdd227d8e11da437aa1e
oktaviannalle <oktavian.putra@formulatrix.com>
2026-08-12 12:24:30 +0700

    feat: add role to fillable and define user relationships

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,18 +4,23 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
 
     /**
      * Get the attributes that should be cast.
@@ -28,5 +33,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function maintenanceLogs()
+    {
+        return $this->hasMany(MaintenanceLog::class, 'technician_id');
+    }
+
+    public function damageReports()
+    {
+        return $this->hasMany(DamageReport::class, 'reported_by');
     }
 }
